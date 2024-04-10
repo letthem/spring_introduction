fc20b903f45760fea3c0cb2ca077f09239ed719f
letthem <suyeon3662@naver.com>
2024-04-10 10:03:48 +0900

    feat: component-scan, 자동 DI 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository; // 회원 리포지토리가 있어야 함
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) { // memberRepository를 직접 내가 new해서 생성하는 게 아닌, 외부에서 넣어주게 바꿈
         this.memberRepository = memberRepository;
     }
